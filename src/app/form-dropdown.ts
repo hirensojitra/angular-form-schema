3570348094b75baab3ad6cedadf45a08120e0fdb
Hiren Sojitra <hirensojitra007@gmail.com>
2022-06-01 12:07:37 +0530

    CSS Update

--- src/app/form-dropdown.ts
+++ src/app/form-dropdown.ts
@@ -1,6 +1,6 @@
 import { FormBase } from './form-base';
 
 export class DropdownFields extends FormBase<string> {
-  //override controlType = 'dropdown';
-  controlType = 'dropdown';
+  override controlType = 'dropdown';
+  // controlType = 'dropdown';
 }
